cde9080853b721ec4238c06bf486b7fbdbffa909
Olivia Garrett <ojg5g8@mail.missouri.edu>
2019-04-21 20:22:54 -0500

    Hamburger menu stuff

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 
 $(document).ready(function() {
   $('.menu-trigger').click(function() {
-    $('div ul').slideToggle(500);
+    $('#container ul').slideToggle(500);
   });//end slide toggle
 
 //When window is resized, the display:none is turned off for the ul so the menu displays on click or touch.
   $(window).resize(function() {
      if (  $(window).width() > 700 ) {
-    $('div ul').removeAttr('style');
+    $('#container ul').removeAttr('style');
    }
   });//end resize
 });//end ready
